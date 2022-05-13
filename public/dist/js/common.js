1001af219d5b70d5457b38c82c447204e7c19041
saebomlee <lsbbom@gmail.com>
2022-05-13 18:16:17 +0900

    헤더 gnb메뉴 관련 스크립트 수정

--- public/dist/js/common.js
+++ public/dist/js/common.js
@@ -71,12 +71,35 @@ jQuery(function () {
       if (!status) {
         $('.search').addClass('is-active');
         $('.type-search').addClass('is-active');
+        $('body').removeClass('is-white');
+        $('body').removeClass('is-black');
+        $('body').addClass('is-white');
         $('.header').addClass('is-bg-white');
         status = true;
       } else {
         $('.search').removeClass('is-active');
         $('.type-search').removeClass('is-active');
-        $('.header').removeClass('is-bg-white');
+
+        if ($(window).scrollTop() > 0) {
+          $('body').removeClass('is-black');
+          $('body').addClass('is-white');
+        } else {
+          $('.header').removeClass('is-bg-white');
+
+          if ($('.main-banner').length > 0 && $('.main-banner__item').length > 1) {
+            if ($('.main-banner__item[data-bg="white"]').hasClass('swiper-slide-active')) {
+              $('body').removeClass('is-black');
+              $('body').addClass('is-white');
+            } else {
+              $('body').removeClass('is-white');
+              $('body').addClass('is-black');
+            }
+          } else {
+            $('body').removeClass('is-black');
+            $('body').addClass('is-white');
+          }
+        }
+
         status = false;
       }
     }
