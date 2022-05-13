55af3f3aaec3f23f6589b38e6ed20d334ab93529
saebomlee <lsbbom@gmail.com>
2022-05-13 14:33:53 +0900

    헤더 gnb메뉴 관련 스크립트 수정

--- public/src/js/common.js
+++ public/src/js/common.js
@@ -862,6 +862,7 @@ jQuery(function () {
         }
     });
 
+
     function pagingOptionChange() {
         if ($('body').hasClass('is-black')) {
             $('.main-banner__pagination').removeClass('is-black');
