388185e2b017863e7432237e125da62b3cc5614f
saebomlee <lsbbom@gmail.com>
2022-05-16 11:41:01 +0900

    메인 배너 관련 스크립트 수정

--- public/src/js/common.js
+++ public/src/js/common.js
@@ -1029,35 +1029,18 @@ jQuery(function () {
     if ($('.main-banner2').length > 0 && $('.main-banner2__item').length > 1) {
         var mainBannerProgressbarOpt = {
             init: function init() {
-                var slide = $(this.$wrapperEl[0]).find(".swiper-slide-active");
-                var bg = slide.data("bg");
-
-                if ($('.main-banner__item[data-bg="white"]').hasClass('swiper-slide-active')) {
-                    $('body').removeClass('is-black');
-                    $('body').addClass('is-white');
+                if ($('.main-banner2__item').find('.swiper-slide-active').data('bg') === 'white') {
+                    $('.main-banner2__pagination').removeClass('is-black');
+                    $('.main-banner2__pagination').addClass('is-white');
                 } else {
-                    $('body').removeClass('is-white');
-                    $('body').addClass('is-black');
+                    $('.main-banner2__pagination').removeClass('is-white');
+                    $('.main-banner2__pagination').addClass('is-black');
                 }
 
-                $('.main-banner__progressbar').removeClass("animate");
-                $('.main-banner__progressbar').removeClass("active");
-                $('.main-banner__progressbar').eq(0).addClass("animate");
-                $('.main-banner__progressbar').eq(0).addClass("active");
-            },
-            slideChange: function slideChange() {
-                var slide = $(this.$wrapperEl[0]).find(".swiper-slide-active");
-                var bg = slide.data("bg");
-
-                if ($('.main-banner__item[data-bg="white"]').hasClass('swiper-slide-active')) {
-                    $('body').removeClass('is-black');
-                    $('body').addClass('is-white');
-                } else {
-                    $('body').removeClass('is-white');
-                    $('body').addClass('is-black');
-                }
-
-                pagingOptionChange();
+                $('.main-banner2__progressbar').removeClass("animate");
+                $('.main-banner2__progressbar').removeClass("active");
+                $('.main-banner2__progressbar').eq(0).addClass("animate");
+                $('.main-banner2__progressbar').eq(0).addClass("active");
             },
             slideChangeTransitionStart: function slideChangeTransitionStart() {
                 $('.main-banner2__progressbar').removeClass("animate");
@@ -1066,6 +1049,15 @@ jQuery(function () {
             },
             slideChangeTransitionEnd: function slideChangeTransitionEnd() {
                 $('.main-banner2__progressbar').eq(0).addClass("animate");
+            },
+            beforeTransitionStart: function beforeTransitionStart() {
+                if ($('.main-banner2__item').find('.swiper-slide-active').data('bg') === 'white') {
+                    $('.main-banner2__progressbar').removeClass('is-black');
+                    $('.main-banner2__progressbar').addClass('is-white');
+                } else {
+                    $('.main-banner2__progressbar').removeClass('is-white');
+                    $('.main-banner2__progressbar').addClass('is-black');
+                }
             }
         };
         var mainBannerOption = {
@@ -1142,9 +1134,6 @@ jQuery(function () {
             },
             on: {
                 init: function init() {
-                    var slide = $(this.$wrapperEl[0]).find(".swiper-slide-active");
-                    var bg = slide.data("bg");
-
                     if ($('.main-banner3__item').find('.swiper-slide-active').data('bg') === 'white') {
                         $('.main-banner3__pagination').removeClass('is-black');
                         $('.main-banner3__pagination').addClass('is-white');
@@ -1167,9 +1156,6 @@ jQuery(function () {
                     $('.main-banner3__progressbar').eq(0).addClass("animate");
                 },
                 beforeTransitionStart: function beforeTransitionStart() {
-                    var slide = $(this.$wrapperEl[0]).find(".swiper-slide-active");
-                    var bg = slide.data("bg");
-
                     if ($('.main-banner3__item').find('.swiper-slide-active').data('bg') === 'white') {
                         $('.main-banner3__pagination').removeClass('is-black');
                         $('.main-banner3__pagination').addClass('is-white');
